e659495bcbf6191df54269257d875382b2dcd3dd
Robert Watts <robw@wonderbly.com>
2023-09-17 23:15:22 +0100

    New repo, no more blot

--- indiekit.config.js
+++ indiekit.config.js
@@ -4,41 +4,89 @@ const process = require("node:process");
 module.exports = {
   /**
    * Set application options
-   * 
+   *
    * These values are used to configure application behaviour.
    * See: https://getindiekit.com/configuration/#application
-   */ 
+   */
   application: {
+    url: "https://indiekit.robwatts.org",
     mongodbUrl: process.env.MONGO_URL,
   },
   /**
    * Add plug-ins.
-   * 
+   *
    * See: https://getindiekit.com/configuration/#plugins
    */
   plugins: [
     "@indiekit/store-github",
     "@indiekit/syndicator-mastodon",
-    "indiekit-preset-blot",
+    "@indiekit/preset-jekyll",
   ],
   /**
    * Set publication options
-   * 
+   *
    * These values are used to configure application behaviour.
    * See: https://getindiekit.com/configuration/#publication
-   */ 
+   */
   publication: {
-    me: "https://robwatts.org"
+    locale: "en-GB",
+    me: "https://robwatts.org",
+    postTypes: [{
+      type: "article",
+      name: "Article",
+      post: {
+        path: "articles/{yyyy}-{MM}-{dd}-{slug}.md",
+        url: "{yyyy}/{DDD}/a{n}/{slug}/"
+      },
+      media: {
+        path: "media/{yyyy}/{DDD}/a{n}/{filename}",
+        url: "{yyyy}/{DDD}/a{n}/{filename}"
+      }
+    }, {
+      type: "note",
+      name: "Note",
+      post: {
+        path: "notes/{yyyy}-{MM}-{dd}-{n}.md",
+        url: "{yyyy}/{DDD}/n{n}/"
+      }
+    }, {
+      type: "photo",
+      name: "Photo",
+      post: {
+        path: "photos/{yyyy}-{MM}-{dd}-{n}.md",
+        url: "{yyyy}/{DDD}/p{n}/"
+      },
+      media: {
+        path: "media/{yyyy}/{DDD}/p{n}/{filename}",
+        url: "{yyyy}/{DDD}/p{n}/{filename}"
+      }
+    }, {
+      type: "bookmark",
+      name: "Bookmark",
+      post: {
+        path: "bookmarks/{yyyy}-{MM}-{dd}-{slug}.md",
+        url: "{yyyy}/{DDD}/b{n}/{slug}/"
+      }
+    }, {
+      type: "reply",
+      name: "Reply",
+      post: {
+        path: "replies/{yyyy}-{MM}-{dd}-{n}.md",
+        url: "{yyyy}/{DDD}/r{n}/"
+      }
+    }],
+    slugSeparator: "_",
+    timeZone: "Europe/London",
   },
   /**
-   * GitHub content store options.
-   * 
-   * Other content stores are available.
-   * See: https://getindiekit.com/plugins/stores
-   */
+    * GitHub content store options.
+    *
+    * Other content stores are available.
+    * See: https://getindiekit.com/plugins/stores
+    */
   "@indiekit/store-github": {
     user: "robertwatts",
-    repo: "blog",
+    repo: "robwatts-content",
     branch: "master",
     token: process.env.GITHUB_TOKEN
   },
